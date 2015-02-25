cbbd0ef561e9b29b37d505c698cd1b94dc24202c
vladfechetecatalin <fechetevladcatalin@gmail.com>
2015-02-25 15:57:38 +0200

    added prefix to routes

--- src/routes.php
+++ src/routes.php
@@ -2,19 +2,20 @@
 if (!isset($routebase) || !isset($viewController) || !isset($postController)) {
 	throw new Exception ('This file can\'t be included outside of ForumServiceProvider@boot!');
 }
+\Route::group(array('prefix' => \LaravelLocalization::setLocale()),  function() use ($routebase, $viewController, $postController){
+	/*
+	 *  Defines routes used by Forum controller
+	 */
+	\Route:: get($routebase, $viewController.'@getIndex');
+	\Route:: get($routebase.'{categoryId}-{categoryUrl}', $viewController.'@getCategory');
+	\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}', $viewController.'@getTopic');
 
-/*
- *  Defines routes used by Forum controller
- */
-\Route:: get($routebase, $viewController.'@getIndex');
-\Route:: get($routebase.'{categoryId}-{categoryUrl}', $viewController.'@getCategory');
-\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}', $viewController.'@getTopic');
+	\Route:: get($routebase.'{categoryId}-{categoryUrl}/new', $postController.'@getNewTopic');
+	\Route::post($routebase.'{categoryId}-{categoryUrl}/new', $postController.'@postNewTopic');
 
-\Route:: get($routebase.'{categoryId}-{categoryUrl}/new', $postController.'@getNewTopic');
-\Route::post($routebase.'{categoryId}-{categoryUrl}/new', $postController.'@postNewTopic');
+	\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/new', $postController.'@getNewMessage');
+	\Route::post($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/new', $postController.'@postNewMessage');
 
-\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/new', $postController.'@getNewMessage');
-\Route::post($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/new', $postController.'@postNewMessage');
-
-\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/edit/{messageId}', $postController.'@getEditMessage');
-\Route::post($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/edit/{messageId}', $postController.'@postEditMessage');
+	\Route:: get($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/edit/{messageId}', $postController.'@getEditMessage');
+	\Route::post($routebase.'{categoryId}-{categoryUrl}/{topicId}-{topicUrl}/edit/{messageId}', $postController.'@postEditMessage');
+});
